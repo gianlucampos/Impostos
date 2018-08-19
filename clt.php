68055ab1730e5ce4eaef1229301664ac58059643
gianluca <gianluca@Gianlucampos>
2018-08-19 17:27:30 -0300

    Arredondamento feito
    --Falta Simples Nacional
    --Css

--- clt.php
+++ clt.php
@@ -6,22 +6,29 @@
         #tabelaImpostos {
             font-family: arial, sans-serif;
             border-collapse: collapse;
-/*            width: 100%;*/
+            /*            width: 100%;*/
         }
 
         td, th {
             border: 1px solid #dddddd;
             text-align: left;
-/*            padding: 8px;*/
+            /*            padding: 8px;*/
         }
 
         tr:nth-child(even) {
             background-color: #dddddd;
         }
+        
+        .output{
+            color: green;
+        }
     </style>
     <body>  
         <div>
             <?php
+            function arredonda($input) {
+                return round($input * 100) / 100;
+            }
             $inss = "";
             $fgts = "";
             $d13salario = "";
@@ -59,18 +66,18 @@
             }
             $salario = isset($_POST['salario']) ? $_POST['salario'] : "";
             if (!empty($salario)) {
-                $inss = 0.278 * $salario; //6
-                $fgts = 0.08 * $salario; //7
-                $d13salario = $salario/12; //8
-                $inss_sem_d13 = 0.278 * $d13salario; //9
-                $fgts_sem_d13 = 0.08 * $d13salario; //10
-                $ferias1_12 = $d13salario; //11
-                $ferias1_3 = $ferias1_12/3; //12
-                $inss_sem_ferias1_12 = 0.278 * $ferias1_12; //13
-                $inss_sem_ferias1_3 = 0.278 * $inss_sem_ferias1_12; //14
-                $fgts_sem_ferias1_12 = 0.08 * $ferias1_12; //15
-                $fgts_sem_ferias1_3 = 0.278 * $ferias1_3; //16
-                $avisoPrevio1_12 = (1 / 12) * $salario; //17
+                $inss = arredonda(0.278 * $salario); //6
+                $fgts = arredonda(0.08 * $salario); //7
+                $d13salario = arredonda($salario / 12); //8
+                $inss_sem_d13 = arredonda(0.278 * $d13salario); //9
+                $fgts_sem_d13 = arredonda(0.08 * $d13salario); //10
+                $ferias1_12 = arredonda($d13salario); //11
+                $ferias1_3 = arredonda($ferias1_12 / 3); //12
+                $inss_sem_ferias1_12 = arredonda(0.278 * $ferias1_12); //13
+                $inss_sem_ferias1_3 = arredonda(0.278 * $inss_sem_ferias1_12); //14
+                $fgts_sem_ferias1_12 = arredonda(0.08 * $ferias1_12); //15
+                $fgts_sem_ferias1_3 = arredonda(0.278 * $ferias1_3); //16
+                $avisoPrevio1_12 = arredonda((1 / 12) * $salario); //17
                 $multaFgts = ($fgts_sem_d13 + $fgts_sem_d13 + $fgts_sem_ferias1_12 + $fgts_sem_ferias1_3) / 2; //18
                 $custoMensal = 
                         $inss +
@@ -80,11 +87,11 @@
                         $fgts_sem_d13 +
                         $ferias1_12 +
                         $ferias1_3 +
-                        $inss_sem_ferias1_12 + 
-                        $inss_sem_ferias1_3 + 
-                        $fgts_sem_ferias1_12 + 
-                        $fgts_sem_ferias1_3 + 
-                        $avisoPrevio1_12 + 
+                        $inss_sem_ferias1_12 +
+                        $inss_sem_ferias1_3 +
+                        $fgts_sem_ferias1_12 +
+                        $fgts_sem_ferias1_3 +
+                        $avisoPrevio1_12 +
                         $multaFgts;
                 $custoMensalSalario = $custoMensal + $salario;
                 $custoHora = $custoMensal / 220;
@@ -101,59 +108,59 @@
                 </tr>
                 <tr>
                     <td>INSS:</td>
-                    <td><?php echo $inss; ?></td>
+                    <td class="output"><?php echo $inss; ?></td>
                 </tr>
                 <tr>
                     <td>FGTS:</td>
-                    <td><?php echo $fgts; ?></td>
+                    <td class="output"><?php echo $fgts; ?></td>
                 </tr>
                 <tr>
                     <td>1/12 13º salário</td>
-                    <td><?php echo $d13salario; ?></td>
+                    <td class="output"><?php echo $d13salario; ?></td>
                 </tr>
                 <tr>
                     <td>INSS sem 13º salário</td>
-                    <td><?php echo $inss_sem_d13; ?></td>
+                    <td class="output"><?php echo $inss_sem_d13; ?></td>
                 </tr>
                 <tr>
                     <td>FGTS sem 13º salário</td>
-                    <td><?php echo $fgts_sem_d13; ?></td>
+                    <td class="output"><?php echo $fgts_sem_d13; ?></td>
                 </tr>
                 <tr>
                     <td>1/12 Férias</td>
-                    <td><?php echo $ferias1_12; ?></td>                
+                    <td class="output"><?php echo $ferias1_12; ?></td>                
                 </tr>
                 <tr>
                     <td>1/3 Férias</td>
-                    <td><?php echo $ferias1_3; ?></td>                
+                    <td class="output"><?php echo $ferias1_3; ?></td>                
                 </tr>
                 <tr>
                     <td>INSS sem 1/12 Férias </td>
-                    <td><?php echo $inss_sem_ferias1_12; ?></td>                
+                    <td class="output"><?php echo $inss_sem_ferias1_12; ?></td>                
                 </tr>
                 <tr>
                     <td>INSS sem 1/3 Férias</td>
-                    <td><?php echo $inss_sem_ferias1_3; ?></td>                
+                    <td class="output"><?php echo $inss_sem_ferias1_3; ?></td>                
                 </tr>
                 <tr>
                     <td>FGTS sem Férias</td>
-                    <td><?php echo $fgts_sem_ferias1_12; ?></td>                
+                    <td class="output"><?php echo $fgts_sem_ferias1_12; ?></td>                
                 </tr>
                 <tr>
                     <td>FGTS sem 1/3 Férias</td>
-                    <td><?php echo $fgts_sem_ferias1_3; ?></td>                
+                    <td class="output"><?php echo $fgts_sem_ferias1_3; ?></td>                
                 </tr>
                 <tr>
                     <td>1/12 Aviso prévio</td>
-                    <td><?php echo $avisoPrevio1_12; ?></td>                
+                    <td class="output"><?php echo $avisoPrevio1_12; ?></td>                
                 </tr>
                 <tr>
                     <td>50% multa FGTS</td>
-                    <td><?php echo $multaFgts; ?></td>                
+                    <td class="output"><?php echo $multaFgts; ?></td>                
                 </tr>
                 <tr>
                     <td>Total custo mensal</td>
-                    <td><?php echo $custoMensalSalario; ?></td>                
+                    <td class="output"><?php echo $custoMensalSalario; ?></td>                
                 </tr>
             </table>
         </div>
